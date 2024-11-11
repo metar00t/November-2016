d1db90dc0b902c7f2824c442a32e2e9140cf8366
David-Paul Adams <david-paul.adams@outlook.de>
2024-11-11 05:47:25 +0100

    the reality checkup

---
+++ november_2016.mjs
@@ -0,0 +1,15 @@
+import promptSync from 'prompt-sync';
+import * as support from './lib/js_outsourced_functions/support.mjs';
+import * as output from './lib/js_output/output.mjs';
+
+const prompt = promptSync();
+
+console.log("THE NOVEMBER 2016 GUIDE TO MAKING PEOPLE FEEL OLD\n");
+
+const age = prompt("how old are you? ");
+
+function timeToFeelOld(age)
+{
+    var thing = support.giveThing(age);
+    output.output(thing);
+}
