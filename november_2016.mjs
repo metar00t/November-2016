9a7f7f0dbc7f221ab9ccb72755b5a079f77321ba
David-Paul Adams <david-paul.adams@outlook.de>
2024-11-12 15:28:34 +0100

    optimization

--- november_2016.mjs
+++ november_2016.mjs
@@ -2,17 +2,19 @@ import promptSync from 'prompt-sync';
 import * as support from './lib/js_outsourced_functions/support.mjs';
 import * as output from './lib/js_output/output.mjs';
 
+function timeToFeelOld(age)
+{
+    var result = support.giveThings(age);
+    return result;
+}
+
 const prompt = promptSync();
 
 console.log("THE NOVEMBER 2016 GUIDE TO MAKING PEOPLE FEEL OLD\n");
 
 const age = prompt("how old are you? ");
 
-function timeToFeelOld(age)
-{
-    var result = support.giveThing(age);
-    return result;
-}
 
-var thing = timeToFeelOld(age);
+
+const thing = timeToFeelOld(age);
 output.speaker(thing);
